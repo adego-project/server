6f82c9bb7102feb166c772d5cd61e894373f5477
hyeonjoonpark <pjjoon1379@gmail.com>
2024-03-02 23:48:04 +0900

    ADD: Security Config

---
+++ src/main/java/com/adego/project/global/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.adego.project.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+      .csrf(AbstractHttpConfigurer::disable)
+      .cors(Customizer.withDefaults())
+      .formLogin(AbstractHttpConfigurer::disable)
+      .httpBasic(AbstractHttpConfigurer::disable)
+      .authorizeHttpRequests(
+        auth -> auth
+          .requestMatchers("/api/**").permitAll()
+          .anyRequest().permitAll()
+      )
+      .sessionManagement(
+        session -> session.sessionCreationPolicy(
+          SessionCreationPolicy.STATELESS
+        )
+      );
+
+    return http.build();
+  }
+}
