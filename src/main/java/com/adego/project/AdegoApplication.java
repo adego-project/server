0a95fa7d1991b066d122d15bc54d41d52da66293
hyeonjoonpark <pjjoon1379@gmail.com>
2024-03-02 22:53:26 +0900

    SET: 프로젝트 세팅

---
+++ src/main/java/com/adego/project/AdegoApplication.java
@@ -0,0 +1,13 @@
+package com.adego.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdegoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AdegoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/adego/project/AdegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adego.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdegoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
