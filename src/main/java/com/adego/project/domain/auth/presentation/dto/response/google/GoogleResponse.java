06e2bf546b2385b136a84fa6d1e60eb263a468ec
hyeonjoonpark <pjjoon1379@gmail.com>
2024-03-04 16:21:25 +0900

    OAuthResponse

---
+++ src/main/java/com/adego/project/domain/auth/presentation/dto/response/google/GoogleResponse.java
@@ -0,0 +1,38 @@
+package com.adego.project.domain.auth.presentation.dto.response.google;
+
+import com.adego.project.domain.auth.presentation.dto.response.OAuth2Response;
+
+import java.util.Map;
+
+public class GoogleResponse implements OAuth2Response {
+  private final Map<String, Object> attribute;
+
+  public GoogleResponse(Map<String, Object> attribute) {
+    this.attribute = attribute;
+  }
+
+  @Override
+  public String provider() {
+    return "google";
+  }
+
+  @Override
+  public String getProviderId() {
+    return attribute.get("sub").toString();
+  }
+
+  @Override
+  public String getEmail() {
+    return attribute.get("email").toString();
+  }
+
+  @Override
+  public String getName() {
+    return attribute.get("name").toString();
+  }
+
+  @Override
+  public String getProfileImage() {
+    return null;
+  }
+}
